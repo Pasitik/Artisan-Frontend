4da90ecbd941e3fa3defaf1c4690903c2f3cca13
nanorisrael <israel.nanor@compozelabs.com>
2023-12-01 11:55:08 +0000

    Add artisan profile forms

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "../features/login/loginSlice";
 import signupReducer from "../features/login/signupSlice";
+import categoryReducer from "../features/categorySlice";
+import artisanProfileReducer from "../features/addArtisanSlice";
 
 export const store = configureStore({
   reducer: {
     users: loginReducer,
     signup: signupReducer,
-  },
+    category: categoryReducer,
+    artisanProfile: artisanProfileReducer
+  }
 });
