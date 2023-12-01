2ad720b102f6086f06fef259b88e54f8865d997a
nanorisrael <israel.nanor@compozelabs.com>
2023-12-01 07:24:22 +0000

    Add artisan address

--- src/components/PrivateRoutes.jsx
+++ src/components/PrivateRoutes.jsx
@@ -3,9 +3,6 @@ import { Outlet, Navigate } from "react-router-dom";
 const PrivateRoutes = () => {
   //const isAuthenticated = useSelector(state=> state.users.token != null)
   const isAuthenticated = localStorage.getItem("authToken") != null;
-
-  //console.log()
-
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
