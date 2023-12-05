9a3aac35bcd8ccca95c6012109cd97f6a71ff590
Pasitik <paasitik@gmail.com>
2023-12-05 14:36:06 +0000

    hero

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,17 +15,17 @@ const NavBar = () => {
     let [open, setOpen] = useState(false)
 
   return (
-    <div className='shadow-md w-full fixed top-0 left-0'>
+    <div className='shadow-md w-full top-0 left-0'>
         <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
             <div className='font-bold text-3xl cursor-pointer flex items-center font-[Poppins] text-gray-800'>
                 <span className=' text-green-400'>Artisan</span>
-                <span className=' text-orange-500'>Bay</span>
+                <span className=' text-orange-400'>Bay</span>
             </div>
             <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-3 cursor-pointer md:hidden'>
                 <FontAwesomeIcon icon={open ? faTimes : faBars} />
             </div>
             <ul className= {`md:flex md:items-center md:pb-0 pb-12 absolute md:static md:bg-white bg-gray-300 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9
-            transition-all duration-500 ease-in ${open ? 'top-20 opacity-100' : 'top-[-490px]'} md:opacity-100`}>
+            transition-all duration-500 ease-in ${open ? 'top-20 opacity-100 z-10' : 'top-[-490px]'} md:opacity-100`}>
                 {Links.map((link) =>{
                     return(
                         <li key = {link.name} className='md:ml-8 text-xl md:my-0 my-7'>
@@ -33,7 +33,7 @@ const NavBar = () => {
                         </li>
                     )
                 })}
-                <button className=' bg-orange-300 text-white font-[Poppins] font-bold py-2 px-6 rounded md:ml-8 hover:bg-orange-500 duration-500'>Get Started</button>
+                <button className=' bg-orange-400 text-white font-[Poppins] font-bold py-2 px-6 rounded md:ml-8 hover:bg-orange-300 duration-500'>Get Started</button>
             </ul>
         </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,29 @@ const Home = () => {
   return (
     <div>
       <NavBar />
-
-      <h1 className="text-3xl font-bold underline text-gray-600">Hello world!</h1>
+      <section id="hero" class="py-5">
+        <div className="contaoner flex flex-wrap items-center justify-center mx-auto mt-10 md:px-12 md:flex-row">
+          <div className="mb-14 lg:mb-0 lg:w-1/2">
+            <h1 className="max-w-xl text-[2.9rem] leading-none text-gray-800 font-bold font-sans text-center lg:text-5xl lg:text-left lg:leading-tight mb-5">
+              Find professional artisans near you with speed and super ease !!!
+            </h1>
+            <p className="max-w-xl text-center text-grey-500 lg:text-left lg:max-w-md">
+              We offer a wide range of services to help you grow your business. Whether it's designing an app, creating a website or providing digital support.
+            </p>
+            <div className="flex justify-center mt-14 lg:justify-start">
+              <button type="button" className="text-white bg-green-500 font-medium rounded-lg px-5 py-4 text-center hover:bg-green-300 houver:drop-shadow-md transition duration-300 ease-in-out">
+                Learn more
+              </button>
+              <button type="button" className="ml-5 text-white bg-orange-400 font-medium rounded-lg px-5 py-4 text-center hover:bg-orange-300 houver:drop-shadow-md transition duration-300 ease-in-out">
+                Join Artisans
+              </button>
+            </div>
+          </div>
+          <div className="ld:w-1/2">
+            <img className="ml-auto h-[70vh] rounded-lg" src="https://images.pexels.com/photos/8817854/pexels-photo-8817854.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="hero-img"/>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
