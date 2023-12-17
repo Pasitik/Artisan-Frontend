8767559cb81080bce0dc408d24743286d1589669
Pasitik <paasitik@gmail.com>
2023-12-17 17:11:01 +0000

    replacing lorem text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ import {
 const About = () => {
   return (
     <section className="w-full md:flex bg-slate-200 md:p-10">
-      <div className="md:w-1/2 hidden md:block">
+      <div className="md:flex md:w-1/2 hidden items-center">
         <img
           className=" rounded-md"
           src="https://images.pexels.com/photos/848205/pexels-photo-848205.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -23,15 +23,26 @@ const About = () => {
           About
         </h1>
         <p className=" text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed velit
-          fugiat cupiditate fugit quos maxime minima modi at magni dicta
-          repellat aliquam iusto fuga, suscipit optio eveniet. Ducimus
-          doloremque, dolores exercitationem cumque est veniam cum corrupti
-          dignissimos, accusamus repudiandae necessitatibus.
+          Welcome to ArtisanBay, your go-to platform for connecting with skilled
+          artisans in your local community. We understand the value of
+          craftsmanship and the importance of finding reliable professionals for
+          your projects, be it carpentry, plumbing, electrical work, or other
+          artisanal services. ArtisanBay simplifies the process of discovering
+          and hiring talented individuals right in your neighborhood. At
+          ArtisanBay, we prioritize quality, reliability, and community
+          collaboration. Our platform not only helps you find the right artisan
+          for your needs but also fosters a sense of community by showcasing the
+          talents of local craftsmen. We believe in the power of connecting
+          people with the skills and expertise they require, promoting a vibrant
+          ecosystem of trades and services.
         </p>
         <p className=" font-semibold font-sans pt-3 text-center">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, libero
-          minima sequi itaque corrupti obcaecati.
+          Join us in supporting local artisans and building a stronger, more
+          connected community. Whether you're a homeowner in search of skilled
+          professionals or an artisan looking to showcase your expertise,
+          ArtisanBay is here to bridge the gap and make the process seamless.
+          Welcome to a community where craftsmanship thrives, and connections
+          are made.
         </p>
         <div className="w-full flex my-10 justify-evenly">
           <Link to="instagram.com ">
